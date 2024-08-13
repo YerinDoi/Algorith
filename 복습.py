f646928fbaee1d0ce24a98549151a40e49ef4c46
yerin <dpfls1022@naver.com>
2024-08-13 23:14:46 +0900

    복습

--- 복습.py
+++ 복습.py
@@ -1,33 +1,31 @@
-array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+def dfs(x, y):
+  if x < 0 or y < 0 or x >= n or y >= m:  # 0부터 시작하기 때문에 x >= n, y >= m 주의
+    return False
+  
+  if graph[x][y] == 0:
+    graph[x][y] = 1
 
-def quick_sort(array, start, end):
-    if start >= end:
-        return
+    dfs(x - 1, y)
+    dfs(x + 1, y)
+    dfs(x, y - 1)
+    dfs(x, y + 1)
 
-    pivot = start
-    left = start + 1
-    right = end
+    return True
+  return False
+    
 
-    while left <= right:
-        # 피벗보다 큰 값을 찾기
-        while left <= end and array[left] <= array[pivot]:
-            left += 1
+input_data = list(map(int, input().split()))
+n = input_data[0]
+m = input_data[1]
 
-        # 피벗보다 작은 값을 찾기
-        while right >= start and array[right] >= array[pivot]:
-            right -= 1
+graph = []
+for i in range(n):
+  graph.append(list(map(int, input())))
 
-        # 수정된 부분: 교환 조건을 수정하여 left와 right가 엇갈렸을 때 피벗 교환
-        if left > right:
-            array[right], array[pivot] = array[pivot], array[right]
-        else:
-            # 수정된 부분: left와 right의 값을 교환하는 부분의 주석
-            array[left], array[right] = array[right], array[left]
+result = 0
+for i in range(n):
+  for j in range(m):
+    if dfs(i, j) == True:
+      result += 1
 
-    # 피벗이 제자리에 왔으니, 피벗 기준으로 분할하여 정렬
-    quick_sort(array, start, right - 1)
-    quick_sort(array, right + 1, end)
-
-quick_sort(array, 0, len(array) - 1)
-
-print(array)
+print(result)
